2408a050b535bcfe92edfecb5422d5ff47073695
antonar <antonar@gmx.com>
2014-02-14 17:24:41 +0100

    Added greeting button.

--- HalloVerden/HalloVerden/Form1.vb
+++ HalloVerden/HalloVerden/Form1.vb
@@ -3,5 +3,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("Hallo, verden!")
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("Hilsen fra Tromsø!")
+    End Sub
 End Class
-s
