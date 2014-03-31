1eeea5cca17b977d5b8d23d815f4eb312fd8b4b3
oseidar <oseidar@gmail.com>
2014-03-31 19:57:20 +0200

    DB oppdatert

---
+++ KakefunnMockups/KakefunnMockups/core/Db.vb
@@ -0,0 +1,34 @@
+﻿Imports MySql.Data.MySqlClient
+
+
+
+Public Class Db
+    Private username As String
+    Private password As String
+    Private db As String
+    Private url As String
+
+
+
+
+
+    Public Sub New()
+        Me.username = "root"
+        Me.password = "w1nd5urf"
+        Me.url = "localhost"
+        Me.db = "L3"
+    End Sub
+
+    Public Function connect() As MySqlConnection
+        Dim con As New MySqlConnection(Me.buildString())
+        Return con
+    End Function
+
+    Private Function buildString() As String
+        Dim Str = "Server=" & Me.url & ";Database=" & Me.db & ";Uid=" & Me.username & ";Pwd=" & Me.password & ";"
+
+        Return Str
+    End Function
+
+
+End Class
--- KakefunnMockups/KakefunnMockups/models/Order.vb
+++ KakefunnMockups/KakefunnMockups/models/Order.vb
@@ -2,8 +2,16 @@
 
     Inherits Model
 
+    Private orderDate As Date
+    Private deliveryDate As Date
 
 
 
 
+
+
+
+    Public Sub New()
+
+    End Sub
 End Class
