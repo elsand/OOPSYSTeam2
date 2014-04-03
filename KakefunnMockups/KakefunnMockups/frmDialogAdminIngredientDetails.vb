59277b9675c670d8b6b96e60a1a9060a1b2f924c
antonar <antonar@gmx.com>
2014-04-03 18:28:40 +0200

    Endringer i frmDialogAdminIngredientDetails.vb

--- KakefunnMockups/KakefunnMockups/frmDialogAdminIngredientDetails.vb
+++ KakefunnMockups/KakefunnMockups/frmDialogAdminIngredientDetails.vb
@@ -1,7 +1,13 @@
 ﻿Public Class frmDialogAdminIngredientDetails
     Private Sub frmDialogAdminIngredientDetails_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim Db As New Db()
+        Dim Db As Db = New Db
 
+        'Populating combobox with unit types on form load.
+        Dim query = From it In Db.units Select it
+        Dim unitType As unit
+        For Each unitType In query
+            ComboBox1.Items.Add(unitType.name)
+        Next
 
     End Sub
 End Class
