f65e8ce230d9614b0ac83b84cc5f3f673d0d9fc1
jpsvedal <jp@svedal.com>
2014-04-04 23:43:29 +0200

    Navn kontroller

--- KakefunnMockups/KakefunnMockups/frmAdminProcessedOrders.Designer.vb
+++ KakefunnMockups/KakefunnMockups/frmAdminProcessedOrders.Designer.vb
@@ -19,31 +19,31 @@ Partial Class frmAdminProcessedOrders
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.dtgProcessedOrders = New System.Windows.Forms.DataGridView()
         Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.btnPrintProcessedOrders = New System.Windows.Forms.Button()
+        Me.btnTransferToBillingSystem = New System.Windows.Forms.Button()
+        CType(Me.dtgProcessedOrders, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'DataGridView1
+        'dtgProcessedOrders
         '
-        Me.DataGridView1.AllowUserToAddRows = False
-        Me.DataGridView1.AllowUserToDeleteRows = False
-        Me.DataGridView1.AllowUserToOrderColumns = True
-        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
-        Me.DataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column5, Me.Column1, Me.Column2, Me.Column3, Me.Column6, Me.Column4})
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 66)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(696, 306)
-        Me.DataGridView1.TabIndex = 5
+        Me.dtgProcessedOrders.AllowUserToAddRows = False
+        Me.dtgProcessedOrders.AllowUserToDeleteRows = False
+        Me.dtgProcessedOrders.AllowUserToOrderColumns = True
+        Me.dtgProcessedOrders.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dtgProcessedOrders.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
+        Me.dtgProcessedOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dtgProcessedOrders.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column5, Me.Column1, Me.Column2, Me.Column3, Me.Column6, Me.Column4})
+        Me.dtgProcessedOrders.Location = New System.Drawing.Point(12, 66)
+        Me.dtgProcessedOrders.Name = "dtgProcessedOrders"
+        Me.dtgProcessedOrders.Size = New System.Drawing.Size(696, 306)
+        Me.dtgProcessedOrders.TabIndex = 5
         '
         'Column5
         '
@@ -75,49 +75,49 @@ Partial Class frmAdminProcessedOrders
         Me.Column4.HeaderText = "Beløp"
         Me.Column4.Name = "Column4"
         '
-        'Button2
+        'btnPrintProcessedOrders
         '
-        Me.Button2.Location = New System.Drawing.Point(12, 378)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(132, 23)
-        Me.Button2.TabIndex = 6
-        Me.Button2.Text = "Skriv ut rapport"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnPrintProcessedOrders.Location = New System.Drawing.Point(12, 378)
+        Me.btnPrintProcessedOrders.Name = "btnPrintProcessedOrders"
+        Me.btnPrintProcessedOrders.Size = New System.Drawing.Size(132, 23)
+        Me.btnPrintProcessedOrders.TabIndex = 6
+        Me.btnPrintProcessedOrders.Text = "Skriv ut rapport"
+        Me.btnPrintProcessedOrders.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnTransferToBillingSystem
         '
-        Me.Button3.Location = New System.Drawing.Point(576, 378)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(132, 23)
-        Me.Button3.TabIndex = 7
-        Me.Button3.Text = "Overfør til fakturasystem"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnTransferToBillingSystem.Location = New System.Drawing.Point(576, 378)
+        Me.btnTransferToBillingSystem.Name = "btnTransferToBillingSystem"
+        Me.btnTransferToBillingSystem.Size = New System.Drawing.Size(132, 23)
+        Me.btnTransferToBillingSystem.TabIndex = 7
+        Me.btnTransferToBillingSystem.Text = "Overfør til fakturasystem"
+        Me.btnTransferToBillingSystem.UseVisualStyleBackColor = True
         '
         'frmAdminProcessedOrders
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.ClientSize = New System.Drawing.Size(720, 441)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnTransferToBillingSystem)
+        Me.Controls.Add(Me.btnPrintProcessedOrders)
+        Me.Controls.Add(Me.dtgProcessedOrders)
         Me.Name = "frmAdminProcessedOrders"
         Me.Text = "Effektuerte ordrer"
-        Me.Controls.SetChildIndex(Me.DataGridView1, 0)
-        Me.Controls.SetChildIndex(Me.Button2, 0)
-        Me.Controls.SetChildIndex(Me.Button3, 0)
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Controls.SetChildIndex(Me.dtgProcessedOrders, 0)
+        Me.Controls.SetChildIndex(Me.btnPrintProcessedOrders, 0)
+        Me.Controls.SetChildIndex(Me.btnTransferToBillingSystem, 0)
+        CType(Me.dtgProcessedOrders, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents dtgProcessedOrders As System.Windows.Forms.DataGridView
     Friend WithEvents Column5 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column3 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column6 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column4 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents btnPrintProcessedOrders As System.Windows.Forms.Button
+    Friend WithEvents btnTransferToBillingSystem As System.Windows.Forms.Button
 
 End Class
--- KakefunnMockups/KakefunnMockups/frmDialogSearchResults.Designer.vb
+++ KakefunnMockups/KakefunnMockups/frmDialogSearchResults.Designer.vb
@@ -19,86 +19,35 @@ Partial Class frmDialogSearchResults
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.dtgSearchResultsOrders = New System.Windows.Forms.DataGridView()
         Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.dtgSearchResultsCustomers = New System.Windows.Forms.DataGridView()
         Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.GroupBox1.SuspendLayout()
+        Me.lblHitsInOrders = New System.Windows.Forms.Label()
+        Me.lblHitsInCustomers = New System.Windows.Forms.Label()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.grpSearchResults = New System.Windows.Forms.GroupBox()
+        CType(Me.dtgSearchResultsOrders, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dtgSearchResultsCustomers, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpSearchResults.SuspendLayout()
         Me.SuspendLayout()
         '
-        'DataGridView1
+        'dtgSearchResultsOrders
         '
-        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
-        Me.DataGridView1.Location = New System.Drawing.Point(18, 42)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowHeadersVisible = False
-        Me.DataGridView1.Size = New System.Drawing.Size(504, 209)
-        Me.DataGridView1.TabIndex = 0
-        '
-        'DataGridView2
-        '
-        Me.DataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
-        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column5, Me.Column6, Me.Column7, Me.Column8})
-        Me.DataGridView2.Location = New System.Drawing.Point(18, 296)
-        Me.DataGridView2.Name = "DataGridView2"
-        Me.DataGridView2.RowHeadersVisible = False
-        Me.DataGridView2.Size = New System.Drawing.Size(504, 209)
-        Me.DataGridView2.TabIndex = 1
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(15, 26)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(81, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "123 treff i ordrer"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(15, 280)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(75, 13)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "4 treff i kunder"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(236, 559)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Lukk"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Controls.Add(Me.DataGridView1)
-        Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Controls.Add(Me.DataGridView2)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(542, 529)
-        Me.GroupBox1.TabIndex = 5
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Søkeresultater etter ""xxxx"""
+        Me.dtgSearchResultsOrders.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dtgSearchResultsOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dtgSearchResultsOrders.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4})
+        Me.dtgSearchResultsOrders.Location = New System.Drawing.Point(18, 42)
+        Me.dtgSearchResultsOrders.Name = "dtgSearchResultsOrders"
+        Me.dtgSearchResultsOrders.RowHeadersVisible = False
+        Me.dtgSearchResultsOrders.Size = New System.Drawing.Size(504, 209)
+        Me.dtgSearchResultsOrders.TabIndex = 0
         '
         'Column1
         '
@@ -120,6 +69,17 @@ Partial Class frmDialogSearchResults
         Me.Column4.HeaderText = "Bestiller"
         Me.Column4.Name = "Column4"
         '
+        'dtgSearchResultsCustomers
+        '
+        Me.dtgSearchResultsCustomers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dtgSearchResultsCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dtgSearchResultsCustomers.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column5, Me.Column6, Me.Column7, Me.Column8})
+        Me.dtgSearchResultsCustomers.Location = New System.Drawing.Point(18, 296)
+        Me.dtgSearchResultsCustomers.Name = "dtgSearchResultsCustomers"
+        Me.dtgSearchResultsCustomers.RowHeadersVisible = False
+        Me.dtgSearchResultsCustomers.Size = New System.Drawing.Size(504, 209)
+        Me.dtgSearchResultsCustomers.TabIndex = 1
+        '
         'Column5
         '
         Me.Column5.HeaderText = "Kundenr"
@@ -140,34 +100,74 @@ Partial Class frmDialogSearchResults
         Me.Column8.HeaderText = "Telefon"
         Me.Column8.Name = "Column8"
         '
+        'lblHitsInOrders
+        '
+        Me.lblHitsInOrders.AutoSize = True
+        Me.lblHitsInOrders.Location = New System.Drawing.Point(15, 26)
+        Me.lblHitsInOrders.Name = "lblHitsInOrders"
+        Me.lblHitsInOrders.Size = New System.Drawing.Size(81, 13)
+        Me.lblHitsInOrders.TabIndex = 2
+        Me.lblHitsInOrders.Text = "123 treff i ordrer"
+        '
+        'lblHitsInCustomers
+        '
+        Me.lblHitsInCustomers.AutoSize = True
+        Me.lblHitsInCustomers.Location = New System.Drawing.Point(15, 280)
+        Me.lblHitsInCustomers.Name = "lblHitsInCustomers"
+        Me.lblHitsInCustomers.Size = New System.Drawing.Size(75, 13)
+        Me.lblHitsInCustomers.TabIndex = 3
+        Me.lblHitsInCustomers.Text = "4 treff i kunder"
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(236, 559)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 4
+        Me.btnClose.Text = "Lukk"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'grpSearchResults
+        '
+        Me.grpSearchResults.Controls.Add(Me.lblHitsInOrders)
+        Me.grpSearchResults.Controls.Add(Me.dtgSearchResultsOrders)
+        Me.grpSearchResults.Controls.Add(Me.lblHitsInCustomers)
+        Me.grpSearchResults.Controls.Add(Me.dtgSearchResultsCustomers)
+        Me.grpSearchResults.Location = New System.Drawing.Point(12, 12)
+        Me.grpSearchResults.Name = "grpSearchResults"
+        Me.grpSearchResults.Size = New System.Drawing.Size(542, 529)
+        Me.grpSearchResults.TabIndex = 5
+        Me.grpSearchResults.TabStop = False
+        Me.grpSearchResults.Text = "Søkeresultater etter ""xxxx"""
+        '
         'frmDialogSearchResults
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.ClientSize = New System.Drawing.Size(566, 600)
-        Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.grpSearchResults)
+        Me.Controls.Add(Me.btnClose)
         Me.Name = "frmDialogSearchResults"
         Me.Text = "Søk etter ""xxxx"""
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
+        CType(Me.dtgSearchResultsOrders, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dtgSearchResultsCustomers, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grpSearchResults.ResumeLayout(False)
+        Me.grpSearchResults.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents dtgSearchResultsOrders As System.Windows.Forms.DataGridView
     Friend WithEvents Column1 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column3 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column4 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents DataGridView2 As System.Windows.Forms.DataGridView
+    Friend WithEvents dtgSearchResultsCustomers As System.Windows.Forms.DataGridView
     Friend WithEvents Column5 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column6 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column7 As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Column8 As System.Windows.Forms.DataGridViewTextBoxColumn
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblHitsInOrders As System.Windows.Forms.Label
+    Friend WithEvents lblHitsInCustomers As System.Windows.Forms.Label
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents grpSearchResults As System.Windows.Forms.GroupBox
 
 End Class
