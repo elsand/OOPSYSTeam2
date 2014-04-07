4687fab467fe876b58e2e33aefde35c86f6eaff8
antonar <antonar@gmx.com>
2014-04-07 21:17:09 +0200

    test

--- KakefunnMockups/KakefunnMockups/My Project/Application.Designer.vb
+++ KakefunnMockups/KakefunnMockups/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Kakefunn.frmAdminCakes
+            Me.MainForm = Global.Kakefunn.frmAdminBatch
         End Sub
     End Class
 End Namespace
