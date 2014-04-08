020835d3b37e4910edb2be2560393c6305bdf6ef
elsand <bjorn@langfors.no>
2014-04-08 13:15:41 +0200

    Lagde grensesnitt i DBM for å hente ut utypet datasets/tables (til bruk i rapporter)

---
+++ KakefunnMockups/KakefunnMockups/ReportTest.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.Reporting.WinForms
+
+Public Class ReportTest
+
+    Dim reportDataSource As ReportDataSource
+
+    Private Sub ReportTest_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim reportDt As DataTable = DBM.Instance.GetDataTableFromQuery( _
+            "SELECT CONCAT('aaa', i.name, 'bbb') AS name, SUM(ol.amount) AS totalAmount, " & _
+            "SUM(ol.totalPrice) AS tPrice, SUM(ol.totalPrice) / SUM(ol.amount) AS pricePrUnit " & _
+            "FROM `Order` o INNER JOIN OrderLine ol ON ol.orderId = o.id " & _
+            "INNER JOIN Ingredient i ON ol.ingredientId = i.id WHERE(DateDiff(Now(), o.created) < 31) " & _
+            "GROUP BY i.id" _
+        )
+
+        reportDataSource = New ReportDataSource("DataSet1", reportDt)
+
+        With rvTest.LocalReport
+            .DataSources.Clear()
+            .DataSources.Add(reportDataSource)
+            .ReportEmbeddedResource = "Kakefunn.Turnover.rdlc"
+        End With
+
+        rvTest.RefreshReport()
+    End Sub
+
+End Class
--- KakefunnMockups/KakefunnMockups/controllers/DatabaseManager.vb
+++ KakefunnMockups/KakefunnMockups/controllers/DatabaseManager.vb
@@ -1,5 +1,8 @@
 ﻿Imports System.Data.Objects
 Imports System.Data.Objects.DataClasses
+Imports System.Data.SqlClient
+Imports System.Data.EntityClient
+Imports MySql.Data.MySqlClient
 
 Public Class DBM
     Inherits Db
@@ -30,12 +33,25 @@ Public Class DBM
         End Get
     End Property
 
+#End Region
 
-    Public Function FindByName(table As String, name As String)
-        Return Database.SqlQuery(Of String)("SELET * FROM " & table & " WHERE name = '" & name & "'").ToList()
+    '    Public Function FindByName(table As String, name As String)
+    '        Return Database.SqlQuery(Of String)("SELET * FROM " & table & " WHERE name = '" & name & "'").ToList()
+    '    End Function
+
+
+    Public Function GetDataSetFromQuery(query As String) As DataSet
+        Dim ds As DataSet = New DataSet()
+        Dim sqlCmd As MySqlCommand = New MySqlCommand(query, Instance.Database.Connection)
+        Dim da As MySqlDataAdapter = New MySqlDataAdapter(sqlCmd)
+        da.Fill(ds)
+        Return ds
     End Function
 
+    Public Function GetDataTableFromQuery(query As String) As DataTable
+        Dim ds As DataSet = GetDataSetFromQuery(query)
+        Return ds.Tables.Item(0)
+    End Function
 
-#End Region
 
 End Class
