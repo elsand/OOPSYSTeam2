24e69cb4df4904ec94875de21638457aeb68f856
elsand <bjorn@langfors.no>
2014-04-09 22:34:54 +0200

    AutocompleteHelper

---
+++ KakefunnMockups/KakefunnMockups/helpers/AutoCompleteHelper.vb
@@ -0,0 +1,25 @@
+﻿Public Class AutoCompleteHelper
+
+    Private acSource As New AutoCompleteStringCollection()
+
+    Public Sub New(data As Array)
+        acSource.AddRange(data)
+    End Sub
+
+    Public Sub UseOn(control As TextBox)
+        With control
+            .AutoCompleteMode = AutoCompleteMode.SuggestAppend
+            .AutoCompleteSource = AutoCompleteSource.CustomSource
+            .AutoCompleteCustomSource = acSource
+        End With
+    End Sub
+
+    Public Sub UseOn(control As ComboBox, data As Array)
+        With control
+            .AutoCompleteMode = AutoCompleteMode.SuggestAppend
+            .AutoCompleteSource = AutoCompleteSource.CustomSource
+            .AutoCompleteCustomSource = acSource
+        End With
+    End Sub
+
+End Class
