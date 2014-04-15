9082b03659a42465a7814e7987d5c2118f618a0d
oseidar <oseidar@gmail.com>
2014-04-15 23:24:53 +0200

    frmDialogAdminNotExported rapport

---
+++ KakefunnMockups/KakefunnMockups/frmDialogAdminNotExported.vb
@@ -0,0 +1,33 @@
+﻿''' <summary>
+''' 
+''' </summary>
+''' <remarks></remarks>
+
+Imports Microsoft.Reporting.WinForms
+
+
+Public Class frmDialogAdminNotExported
+    Public reportDataSource As ReportDataSource
+    Private Sub frmDialogAdminNotExported_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim query = "SELECT id,deliveryFirstName,deliveryLastName, FROM `order` WHERE exported = NULL ;"
+
+
+        Dim t = Kakefunn.DBM.Instance.GetDataTableFromQuery(query)
+
+
+
+
+
+
+
+        reportDataSource = New ReportDataSource("DataSet1", t)
+        With Me.rptNotExportedOrders.LocalReport
+            .DataSources.Clear()
+            .DataSources.Add(reportDataSource)
+            .ReportEmbeddedResource = "notExportedOrders.rdlc"
+        End With
+        Me.rptNotExportedOrders.LocalReport.Refresh()
+
+
+    End Sub
+End Class
