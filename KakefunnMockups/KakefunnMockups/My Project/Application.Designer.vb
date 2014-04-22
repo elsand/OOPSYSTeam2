dab06d8558b7f2900f5f3ab145dfcfc506338dab
antonar <antonar@gmx.com>
2014-04-22 06:25:16 +0200

    Oppdatert kommentarer på frmAdminIngredient, samt endret fra Date.Today til Date.Now på sletting av ingredienser.

--- KakefunnMockups/KakefunnMockups/My Project/Application.Designer.vb
+++ KakefunnMockups/KakefunnMockups/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Kakefunn.frmAdminCakes
+            Me.MainForm = Global.Kakefunn.frmAdminIngredient
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
--- KakefunnMockups/KakefunnMockups/frmAdminIngredient.vb
+++ KakefunnMockups/KakefunnMockups/frmAdminIngredient.vb
@@ -1,30 +1,45 @@
-﻿'Ingredient overview.
-'Last edited 13.04.14.
-'OK to finalize?
+﻿''' <summary>
+''' Shows an ingredient overview with id, name, number in stock, price info and profit.
+''' Doubleclick opens an editing window. "Ny ingrediens..." opens the same window for registering a new ingredient.
+''' Last edited 22.04.14.
+''' </summary>
+''' <remarks></remarks>
 Public Class frmAdminIngredient
     Private delIdx As Integer
     Private delName As String
 
+    ''' <summary>
+    ''' Set up autocomplete for ingredient name.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
     Private Sub frmAdminIngredient_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Set up autocomplete for ingredient name
         Dim ac As AutoCompleteHelper = New AutoCompleteHelper(DBM.Instance.GetNameColumn("Ingredient"))
         ac.UseOn(txtSearch)
-
     End Sub
 
+    ''' <summary>
+    ''' Opens frmDialogAdminIngredientDetails for adding new ingredients to the database.
+    ''' Disables the batch-overview in the dialog and sets newIngr true.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles btnNew.Click
-        'Opens frmDialogAdminIngredientDetails for adding new ingredients to the database.
-        '
-        Dim frm As frmDialogAdminIngredientDetails
-        frm = New frmDialogAdminIngredientDetails()
-        'Disables the batch-overview in the dialog and sets newIngr true.
-        frm.grpStock.Enabled = False
-        frm.newIngr = True
-        frm.Show()
+        frmDialogAdminIngredientDetails.grpStock.Enabled = False
+        frmDialogAdminIngredientDetails.newIngr = True
+        SessionManager.Instance.ShowDialog(frmDialogAdminIngredientDetails)
+
     End Sub
 
+    ''' <summary>
+    ''' Searches for ingredients. See detailed comments in sub.
+    ''' </summary>
+    ''' <remarks>
+    ''' Fills datagridview manually using a for loop. May cause performance issues with many ingredients.
+    '''</remarks>
     Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
-        'Searches for ingredients.
         UpdateActionStatus("Søker..")
 
         'Preloading information from tables in the database.
@@ -38,11 +53,11 @@ Public Class frmAdminIngredient
         Dim batchQuery = (From x In DBM.Instance.Batches _
                           Select x).ToList()
 
-        'Filling dtgResults the old fashioned way..
+        'Filling dtgResults.
         UpdateActionStatus("Laster søkeresultat..")
         dtgResults.Rows.Clear()
         For Each row In ingQuery
-            'Gets markupPercentage from db.
+            'Gets markupPercentage from preloaded db data.
             Dim profit As Double = (From x In profQuery _
                                    Where x.id = row.id _
                                    Order By x.date Descending
@@ -62,30 +77,35 @@ Public Class frmAdminIngredient
                                 bdgi, bdgu, profit, row.deleted)
         Next
 
+        'Resets deletion variables.
         delIdx = 0
         delName = ""
         UpdateActionStatus()
     End Sub
 
+    ''' <summary>
+    ''' Opens dialog to change ingredient details on doubleclicking an ingredient in the list.
+    ''' </summary>
+    ''' <remarks></remarks>
     Private Sub dtgResults_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles dtgResults.MouseDoubleClick
-        'Opens dialog to change ingredient details on doubleclicking an ingredient in the list.
         Dim varenr As Integer = dtgResults.SelectedRows(0).Cells(0).Value
         frmDialogAdminIngredientDetails.varenr = varenr
         frmDialogAdminIngredientDetails.newIngr = False
         SessionManager.Instance.ShowDialog(frmDialogAdminIngredientDetails)
     End Sub
 
+    ''' <summary>
+    ''' Marks ingredient as deleted on todays date, after user confirms deletion. 
+    ''' For reporting purposes the deleted ingredient is kept in the database.
+    ''' After deletion reloads the ingredient list.
+    ''' </summary>
+    ''' <remarks></remarks>
     Private Sub btnDel_Click(sender As Object, e As EventArgs) Handles btnDel.Click
-        'Deletes an ingredient
-
-        'Gets ingredient id/varenr and ingredient name.
         If dtgResults.Rows.Count() > 0 Then
             delIdx = dtgResults.SelectedRows(0).Cells(0).Value
             delName = dtgResults.SelectedRows(0).Cells(1).Value
         End If
 
-        'Marks ingredient as deleted on todays date. 
-        'Does not delete the record from db. Prompts the user for confirmation.
         If delIdx > 0 Then
             Dim delOK As Integer = MessageBox.Show("Er du sikker på at du vil slette varen >>" & delName & _
                                                    "<< med varenummer >>" & delIdx & "<<?", "Bekreft sletting", _
@@ -94,7 +114,7 @@ Public Class frmAdminIngredient
                 Dim delIng = (From x In DBM.Instance.Ingredients _
                               Where x.id = delIdx _
                               Select x).FirstOrDefault()
-                delIng.deleted = Date.Today
+                delIng.deleted = Date.Now
 
                 DBM.Instance.SaveChanges()
 
@@ -104,14 +124,15 @@ Public Class frmAdminIngredient
         End If
     End Sub
 
+    ''' <summary>
+    ''' Checks date on the hidden deleted column in the datagridview and formats cells to show which ingredients are deleted.
+    ''' </summary>
+    ''' <remarks></remarks>
     Private Sub dtgResults_CellFormatting(sender As Object, e As DataGridViewCellFormattingEventArgs) Handles dtgResults.CellFormatting
-        'Indicates a deleted ingredient in the list.
-        If dtgResults.Rows(e.RowIndex).Cells(6).Value <= Date.Today And _
-            dtgResults.Rows(e.RowIndex).Cells(6).Value > CDate("2000-01-01") Then
+        If dtgResults.Rows(e.RowIndex).Cells(6).Value <= Date.Now And _
+            dtgResults.Rows(e.RowIndex).Cells(6).Value > CDate("2000-01-01 00:00:00") Then
             dtgResults.Rows(e.RowIndex).DefaultCellStyle.ForeColor = Color.Red
             dtgResults.Rows(e.RowIndex).DefaultCellStyle.Font = New Font(Font, FontStyle.Strikeout)
         End If
-
     End Sub
-
 End Class
