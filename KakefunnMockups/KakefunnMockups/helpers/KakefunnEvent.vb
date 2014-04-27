e1f2e83a5a94235f8c0f7114c6671a02c00fbc79
antonar <antonar@gmx.com>
2014-04-27 10:24:01 +0200

    Kommentert KakefunnEvent.vb.

--- KakefunnMockups/KakefunnMockups/helpers/KakefunnEvent.vb
+++ KakefunnMockups/KakefunnMockups/helpers/KakefunnEvent.vb
@@ -1,18 +1,16 @@
 ﻿''' <summary>
-''' SystemEvent logging
+''' SystemEvent logging. 
+''' saveSystemEvent is called where there is a write operation to the database, 
+''' as well as on attemptet logins.
 ''' </summary>
 ''' <remarks></remarks>
 Public Class KakefunnEvent
     Private logEventName As String
     Private logEventData As String
-
     Public Shared Sub saveSystemEvent(ByVal logEventName As String, logEventData As String)
-
         DBM.Instance.GetDataSetFromQuery( _
          "INSERT INTO SystemEvent (eventName, eventTime, employeeId, eventData)" & _
          "VALUES ('" & logEventName & "', NOW(),'" & SessionHelper.Instance.User.id & "', '" & logEventData & "')" _
          )
-
     End Sub
-
 End Class
