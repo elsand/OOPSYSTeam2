0da42e111d27cd60b06cce7f31424fbb150edfea
antonar <antonar@gmx.com>
2014-04-27 18:21:21 +0200

    Endret så utløpte varer vises selv om det finnes varer med expiredate null.

--- KakefunnMockups/KakefunnMockups/frmLogisticsReports.vb
+++ KakefunnMockups/KakefunnMockups/frmLogisticsReports.vb
@@ -14,12 +14,13 @@
         'Loads the batches
         Try
             DBM.Instance.Batches.Load()
-            BatchBindingSource.DataSource = DBM.Instance.Batches.Local.ToBindingList.Where(Function(b) b.expires.Value.CompareTo(Me.expireDate) <= 0 And b.deleted Is Nothing)
+            BatchBindingSource.DataSource = DBM.Instance.Batches.Local.ToBindingList.Where(Function(b) b.deleted Is Nothing _
+                                                                                               And (b.expires.HasValue _
+                                                                                                    AndAlso b.expires.Value.CompareTo(Me.expireDate) <= 0))
 
             If BatchBindingSource.Count > 1 Then
                 dtgExpiredIngredients.Enabled = True
                 dtgExpiredIngredients.DataSource = BatchBindingSource
-
             End If
             dtgExpiredIngredients.Refresh()
 
